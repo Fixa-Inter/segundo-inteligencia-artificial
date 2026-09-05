39a4e823fd6cfc830a99241f6141d68227d1fcbd
Lorenzo Lima de Oliveira <lorenzolimadeoliveira2010@gmail.com>
2026-09-05 19:52:00 -0300

    feat: Adiciona primeira versão dos esquemas para o resultado dos agentes

---
+++ resultadoAgentes/__init__.py
@@ -0,0 +1,16 @@
+from .AnalyticsResultado import AnalyticsResultado
+from .FAQResultado import FAQResultado
+from .FeedbackResultado import FeedbackResultado
+from .JuizResultado import JuizResultado
+from .SolicitacaoOcorrenciaResultado import SolicitacaoOcorrenciaResultado
+from .VisualizacaoResultado import VisualizacaoResultado
+
+
+__all__ = [
+    "AnalyticsResultado",
+    "FAQResultado",
+    "FeedbackResultado",
+    "JuizResultado",
+    "SolicitacaoOcorrenciaResultado",
+    "VisualizacaoResultado",
+]
